4630a0bfeb5cb9e3b3795cfe279bf2b5b7d5be90
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-07 17:28:14 -0300

    feat: add eslint

--- src/database/config/config.ts
+++ src/database/config/config.ts
@@ -1,17 +1,17 @@
-export {}
+export {};
 require('dotenv/config');
 
 const config = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'DB_USER_MANAGEMENT',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
+	username: process.env.DB_USER || 'root',
+	password: process.env.DB_PASS || '123456',
+	database: 'DB_USER_MANAGEMENT',
+	host: process.env.DB_HOST || 'localhost',
+	port: Number(process.env.DB_PORT) || 3306,
+	dialect: 'mysql',
+	dialectOptions: {
+		timezone: 'Z',
+	},
+	logging: false,
 };
 
 module.exports = config;
--- src/database/models/Users.ts
+++ src/database/models/Users.ts
@@ -2,41 +2,41 @@ import { Model, INTEGER, STRING } from 'sequelize';
 import db from './index';
 
 class User extends Model {
-  declare readonly id: number;
-  declare name: string;
-  declare role: string;
-  declare email: string;
-  declare password: string;
+	declare readonly id: number;
+	declare name: string;
+	declare role: string;
+	declare email: string;
+	declare password: string;
 }
 
 User.init({
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: INTEGER,
-  },
-  name: {
-    allowNull: false,
-    type: STRING,
-  },
-  email: {
-    allowNull: false,
-    type: STRING,
-  },
-  password: {
-    allowNull: false,
-    type: STRING,
-  },
-  role: {
-    allowNull: false,
-    type: STRING,
-  },
+	id: {
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+		type: INTEGER,
+	},
+	name: {
+		allowNull: false,
+		type: STRING,
+	},
+	email: {
+		allowNull: false,
+		type: STRING,
+	},
+	password: {
+		allowNull: false,
+		type: STRING,
+	},
+	role: {
+		allowNull: false,
+		type: STRING,
+	},
 }, {
-  sequelize: db,
-  underscored: true,
-  timestamps: false,
-  modelName: 'users',
+	sequelize: db,
+	underscored: true,
+	timestamps: false,
+	modelName: 'users',
 });
 
 export default User;
