15e1c2a5838f3bed87609c43d5950c9b2caba046
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-07 17:02:29 -0300

    feat(database/models): create representation of the users table

---
+++ src/database/models/Users.ts
@@ -0,0 +1,42 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from './index';
+
+class User extends Model {
+  declare readonly id: number;
+  declare name: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'users',
+});
+
+export default User;
