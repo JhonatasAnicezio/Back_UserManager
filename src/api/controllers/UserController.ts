22e3f5c50508d40d6d0a7f5764fee2b58bbd1bb6
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-08 16:03:49 -0300

    feat: create func for register users

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -12,6 +12,19 @@ class UserController {
     const { type, message } = await this._service.postLogin(req.body);
     return res.status(type).json(message);
   }
+
+  async postRegister(req: Request, res: Response) {
+    const { password, email } = req.body;
+    const newUser = await this._service.postRegister(req.body);
+  
+    if (newUser.type === 201) {
+      const { message } = await this._service.postLogin({ email, password });
+  
+      return res.status(newUser.type).json(message);
+    }
+
+    return res.status(newUser.type).json({ message: newUser.message });
+  };
 }
 
 export default UserController;
