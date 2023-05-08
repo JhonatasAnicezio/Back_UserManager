1fd7772761b436130e5005feb47c2f76c103b8fb
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-08 20:57:09 -0300

    feat: create middleware for validade fields in endpoint login

---
+++ src/api/middlewares/validateFieldsUser.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+class validateFieldsUser {
+  public static verify(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).send({ message: 'All fields must be filled' });
+    }
+
+    next();
+  }
+}
+
+export default validateFieldsUser;
