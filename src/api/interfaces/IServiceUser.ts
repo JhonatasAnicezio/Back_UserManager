a5155501a1cd930335c50a92e09cb0f8b95b358a
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-08 16:00:41 -0300

    feat: add type Login

--- src/api/interfaces/IServiceUser.ts
+++ src/api/interfaces/IServiceUser.ts
@@ -1,6 +1,12 @@
 import User from '../../database/models/Users';
 import IMessage from './IUtils';
 
+export type Login = {
+  email: string,
+  password: string,
+}
+
 export default interface IServiceUser {
-  postLogin(user: User):Promise<IMessage>
+  postLogin(user: Login):Promise<IMessage>
+  postRegister(user: User):Promise<IMessage>
 }
