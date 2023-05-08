29f25df115d0438588923646581298cb17ee335a
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-08 12:04:41 -0300

    feat: create class for jwt

---
+++ src/api/utils/JWTUtil.ts
@@ -0,0 +1,28 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
+
+interface iTokenPayload {
+  email: string;
+  password: string;
+}
+
+class JWTUtil {
+  private _secret = process.env.JWT_SECRET as string;
+
+  generateToken(payload: iTokenPayload): string {
+    const config: jwt.SignOptions = {
+      expiresIn: '3d',
+      algorithm: 'HS256',
+    };
+
+    const token = jwt.sign({ payload }, this._secret, config);
+    return token;
+  }
+
+  verifyToken(token: string): iTokenPayload {
+    const payload = jwt.verify(token, this._secret) as iTokenPayload;
+    return payload;
+  }
+}
+
+export default JWTUtil;
