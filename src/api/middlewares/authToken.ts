c2de1b99740f5987407c0df26c36d932cd5bb84c
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-08 20:59:11 -0300

    feat: create middleware for verify token and admin

---
+++ src/api/middlewares/authToken.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import JWTUtil from '../utils/JWTUtil';
+
+const jwtService = new JWTUtil();
+
+class authToken {
+  public static verify(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    try {
+      const { payload } = jwtService.verifyToken(authorization);
+      console.log(payload);
+      req.body.token = payload;
+      return next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+  }
+
+  public static verifyAdmin(req: Request, res: Response, next: NextFunction) {
+    const { token: { payload } } = req.body;
+    console.log(payload)
+
+    if (!payload) {
+      return res.status(401).json({ message: 'Token not found in admin' });
+    }
+
+    if(payload.role != 'admin') {
+      return res.status(401).json({ message: 'Is not admin' });
+    }
+
+    return next();
+  }
+}
+
+export default authToken;
